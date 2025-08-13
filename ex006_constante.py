f0d4933d6710183e8cecb3b4ba5430e3aa9aecd2
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-08-13 17:13:44 -0300

    Enviando exercício 6

---
+++ ex006_constante.py
@@ -0,0 +1,12 @@
+def const_velocidade():
+    VELOCIDADE_DA_LUZ_NO_VACUO = 299792458 #Velocidade da luz em metros por segundo
+    print(f'Velocidade da luz: {VELOCIDADE_DA_LUZ_NO_VACUO}')
+    try:
+         VELOCIDADE_DA_LUZ_NO_VACUO = 3000000000000
+
+if __name__ == "__main__":
+    const_velocidade()
+
+
+
+
