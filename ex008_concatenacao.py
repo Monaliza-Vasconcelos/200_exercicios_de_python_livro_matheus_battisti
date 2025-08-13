f97a32de29ca76cb473e7e6bb3522daa1c4b30cb
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-08-13 17:35:39 -0300

    Enviando exercício 8

---
+++ ex008_concatenacao.py
@@ -0,0 +1,7 @@
+def concatenacao():
+    nome = str(input('Digite seu nome: '))
+    sobrenome = str(input('Digite seu sobrenome: '))
+    print(f'Seja bem-vindo(a) {nome} {sobrenome}')
+
+if __name__ == "__main__":
+    concatenacao()
