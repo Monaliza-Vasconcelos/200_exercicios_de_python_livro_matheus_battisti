0a7dfd38aeb28d9e610356bc0f07cf2533085fa5
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-08-13 17:45:12 -0300

    Enviando exercício 9

---
+++ ex009_tipos_variaveis.py
@@ -0,0 +1,18 @@
+var_global = str('Olá pessoal') #Variável global
+def var_local():
+    n = str('N') #Variável local
+    print(f'A variável {n} é local, pois só poderá ser utilizada dentro da função')
+    print(f'Já a variável global pode ser utilizada em todo o código, {var_global}')
+
+
+if __name__ == "__main__":
+    var_local()
+
+
+print(f'Já a variável global pode ser utilizada em todo o código, {var_global}')
+
+"""
+    ->Caso adicione a variável n que está dentro da função aqui no programa principal
+    vai retornar um NameError, pois a variável só vai existir enquanto a função estiver
+    rodando...
+"""
