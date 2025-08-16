7eeaffdd59439b1469603557036a12623c6c47e0
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-08-15 21:13:15 -0300

    enviando ex10

--- ex009_tipos_variaveis.py
+++ ex009_tipos_variaveis.py
@@ -1,9 +1,18 @@
 class Tipos_de_variaveis():
     #Variável global
-    nome = 'Helena'
-    def __init__(self,nome):
-        self.nome = nome #Variável de escopo local
-        print(nome)
+    varglobal = 'Helena'
+    def mostrar_valores(self):
+        nome = 'Carla' #Variável de escopo local
+        print(f'Valor da variável global: {self.varglobal}')
+        print(f'Valor da variável local: {nome}')
+
+
+if __name__ == "__main__":
+    variaveis = Tipos_de_variaveis()
+    variaveis.mostrar_valores()
+
+
+
 
 
 
---
+++ ex010_formatação_de_saída.py
@@ -0,0 +1,3 @@
+n = 13.456783
+#Exibe o valor formatado com duas casas decimais.
+print(f'Formatando o valor {n:.2f}')
